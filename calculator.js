d93c87a5dc703a104222e12d69325da0787c1e5a
William <williammutugi7@gmail.com>
2024-01-20 12:59:28 +0300

    Create operate function that calls math functions

--- calculator.js
+++ calculator.js
@@ -13,3 +13,22 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+let operandOne;
+let operandTwo;
+let operator;
+
+function operate (operator, operandOne, operandTwo) {
+    if (operator == "+") {
+        add(operandOne, operandTwo);
+    }
+    if (operator == "-") {
+        subtract(operandOne, operandTwo);
+    }
+    if (operator == "*") {
+        multiply(operandOne, operandTwo);
+    }
+    if (operator == "/") {
+        divide (operandOne, operandTwo);
+    }
+}
