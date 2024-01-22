7b337c4534189d67827a6d2b52a599fd74e7fb09
William <williammutugi7@gmail.com>
2024-01-22 21:41:52 +0300

    Create functions to display numbers & symbols

--- calculator.js
+++ calculator.js
@@ -25,7 +25,7 @@ function operate (operator, operandOne, operandTwo) {
     if (operator == "-") {
         subtract(operandOne, operandTwo);
     }
-    if (operator == "*") {
+    if (operator == "x") {
         multiply(operandOne, operandTwo);
     }
     if (operator == "/") {
@@ -35,66 +35,140 @@ function operate (operator, operandOne, operandTwo) {
 
 let display = document.querySelector("#display");
 
-function display1 () {
-    
+
+operandOne = function () {
+
     document.querySelector("#no1").addEventListener("click", () => {
         display.textContent = no1.textContent;
-    })};
+    });
 
-function display2 () {
+
     document.querySelector("#no2").addEventListener("click", () => {
         display.textContent = no2.textContent;
-    })};
+    });
 
-function display3 () {
+
+
     document.querySelector("#no3").addEventListener("click", () => {
         display.textContent = no3.textContent;
-    })};
+    });
 
-function display4 () {
+
     document.querySelector("#no4").addEventListener("click", () => {
         display.textContent = no4.textContent;
-    })};
+    });
 
-function display5 () {
+
     document.querySelector("#no5").addEventListener("click", () => {
         display.textContent = no5.textContent;
-    })};
+    });
 
 
-function display6 () {
+
     document.querySelector("#no6").addEventListener("click", () => {
             display.textContent = no4.textContent;
-    })};
+    });
 
-function display7 () {
+
     document.querySelector("#no7").addEventListener("click", () => {
             display.textContent = no4.textContent;
-    })};
+    });
 
-function display8 () {
+
     document.querySelector("#no8").addEventListener("click", () => {
         display.textContent = no8.textContent;
-    })};
+    });
 
-function display9 () {
+
     document.querySelector("#no9").addEventListener("click", () => {
         display.textContent = no9.textContent;
-    })};
+     });
 
-function display0 () {
+
     document.querySelector("#no0").addEventListener("click", () => {
         display.textContent = no0.textContent;
-    })};
+     });
+}
+
+operandOne();
+
+operandTwo = function () {
+    document.querySelector("#no1").addEventListener("click", () => {
+        display.textContent = no1.textContent;
+    });
 
 
-display1();
-display2();
-display3();
-display4();
-display5();
-display6();
-display7();
-display8();
-display9();
-display0();
+    document.querySelector("#no2").addEventListener("click", () => {
+        display.textContent = no2.textContent;
+    });
+
+
+
+    document.querySelector("#no3").addEventListener("click", () => {
+        display.textContent = no3.textContent;
+    });
+
+
+    document.querySelector("#no4").addEventListener("click", () => {
+        display.textContent = no4.textContent;
+    });
+
+
+    document.querySelector("#no5").addEventListener("click", () => {
+        display.textContent = no5.textContent;
+    });
+
+
+
+    document.querySelector("#no6").addEventListener("click", () => {
+            display.textContent = no4.textContent;
+    });
+
+
+    document.querySelector("#no7").addEventListener("click", () => {
+            display.textContent = no4.textContent;
+    });
+
+
+    document.querySelector("#no8").addEventListener("click", () => {
+        display.textContent = no8.textContent;
+    });
+
+
+    document.querySelector("#no9").addEventListener("click", () => {
+        display.textContent = no9.textContent;
+     });
+
+
+    document.querySelector("#no0").addEventListener("click", () => {
+        display.textContent = no0.textContent;
+     });
+}
+
+operandTwo();
+
+function displaySymbols () {
+
+    document.querySelector ("#point").addEventListener("click", () => {
+        display.textContent = point.textContent;
+    });
+
+    document.querySelector ("#plus").addEventListener("click", () => {
+        display.textContent = plus.textContent;
+    });
+
+    document.querySelector ("#minus").addEventListener("click", () => {
+        display.textContent = minus.textContent;
+    });
+
+    document.querySelector ("#times").addEventListener("click", () => {
+        display.textContent = times.textContent;
+    });
+
+    document.querySelector ("#division").addEventListener("click", () => {
+        display.textContent = division.textContent;
+    });
+}
+
+displaySymbols();
+
